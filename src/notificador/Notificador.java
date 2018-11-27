80ee4641dc186f7281975920c057fa12846846ee
Gonzalo <gonz.penaloza@alumnos.duoc.cl>
2018-11-26 23:45:23 -0400

    Rama Prueba

--- src/notificador/Notificador.java
+++ src/notificador/Notificador.java
@@ -15,7 +15,7 @@ public class Notificador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // rama prueba
     }
     
 }
